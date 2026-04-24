672e44ff78b13a8cc7f2bd4d2c0c0701ea2642c8
neonhdz <119765490+neonhdz@users.noreply.github.com>
2026-04-24 11:56:21 -0600

    Error Precision Decimal en java

---
+++ Tema1/ErrorPrecisionDecimal.java
@@ -0,0 +1,11 @@
+public class ErrorPrecisionDecimal {
+    public static void main(String[] args) {
+        double a = 0.1;
+        double b = 0.2;
+        double sum = a + b;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("a + b: " + sum);
+    }
+}
