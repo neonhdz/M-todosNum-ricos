b51cb0d57d5d32741d6a7b9a57b433d6e5f478ee
neonhdz <119765490+neonhdz@users.noreply.github.com>
2026-05-27 02:15:59 -0600

    Add ErrorRedondeoBinario.py to demonstrate floating point issues

---
+++ Tema1/ErrorRedondeoBinario.py
@@ -0,0 +1,9 @@
+# Operación aparentemente simple
+a = 0.1 # [cite: 11]
+b = 0.2 # [cite: 12]
+
+# Suma de números decimales
+resultado = a + b # [cite: 14]
+
+# Impresión del resultado (Imprimirá 0.30000000000000004)
+print(resultado) # [cite: 20]
