d93f66bb31806de56960c17a1334cf70586a51b3
neonhdz <119765490+neonhdz@users.noreply.github.com>
2026-05-27 02:19:21 -0600

    Create Truncamiento.py

---
+++ Tema1/Truncamiento.py
@@ -0,0 +1,5 @@
+valor_float = 3.14159e10 # Un número muy grande [cite: 173]
+valor_int = int(valor_float) # Casting explícito [cite: 173]
+
+print("Valor Original:", valor_float) # [cite: 174]
+print("Valor Truncado:", valor_int) # [cite: 174]
